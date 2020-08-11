cf46f5f6fc3806bc3bcb28b499894ab78284205f
gillchristian <gillchristiang@gmail.com>
2020-08-11 08:28:47 +0200

    feat(core): Refactor Effect & move config reading to main

--- src/config.ts
+++ src/config.ts
@@ -1,35 +1,38 @@
-/**
- * @since 0.3.0
- */
 import * as t from 'io-ts'
 
 /**
- * @since 0.3.0
+ * @since 0.6.0
  */
 export const PartialConfig = t.partial({
-  strict: t.boolean
+  strict: t.boolean,
+  outDir: t.string,
+  rootDir: t.string
 })
 
 /**
- * @since 0.3.0
+ * @since 0.6.0
  */
 export interface PartialConfig extends t.TypeOf<typeof PartialConfig> {}
 
 /**
- * @since 0.3.0
+ * @since 0.6.0
  */
 export interface Config {
   strict: boolean
+  outDir: string
+  rootDir: string
 }
 
 /**
- * @since 0.3.0
+ * @since 0.6.0
  */
 export const merge = (partial: PartialConfig, config: Config): Config => ({
-  strict: partial.strict === undefined ? config.strict : partial.strict
+  strict: partial.strict === undefined ? config.strict : partial.strict,
+  outDir: partial.outDir === undefined ? config.outDir : partial.outDir,
+  rootDir: partial.rootDir === undefined ? config.rootDir : partial.rootDir
 })
 
 /**
- * @since 0.3.0
+ * @since 0.6.0
  */
-export const defaultConfig: Config = { strict: false }
+export const defaultConfig: Config = { strict: false, outDir: 'docs', rootDir: 'src' }
